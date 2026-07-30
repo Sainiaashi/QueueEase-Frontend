be58756c0521ada102c583ab4dd62822ae22c938
Sainiaashi <aashisaini27012006@gmail.com>
2026-07-30 20:13:07 +0530

    fix: resolve App component build issue

--- src/App.jsx
+++ src/App.jsx
@@ -20,17 +20,6 @@ import MyTable from './pages/MyTable';
 
 import { ToastProvider } from './context/ToastContext';
 
-function App() {
-  return (
-    <ToastProvider>
-      <AuthProvider>
-        <BrowserRouter>
-          {/* ...everything else stays exactly the same... */}
-        </BrowserRouter>
-      </AuthProvider>
-    </ToastProvider>
-  );
-}
 
 function App() {
   return (
